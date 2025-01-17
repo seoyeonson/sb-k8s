2eb454605f4b8fe2bb81298314a04d3febac9d3e
seoyeonson <eo970721@gmail.com>
2025-01-17 10:50:38 +0900

    work

---
+++ src/test/java/com/ll/k8s/K8sApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ll.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("Test")
+class K8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
